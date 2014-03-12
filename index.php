7e04d061044be818c3670a45275720073a3891e8
Nattu <nattu@Macintosh.local>
2014-03-13 01:17:09 +0500

    Initial Commit. Under construction

---
+++ index.php
@@ -0,0 +1,10 @@
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Kickstart.mv</title>
+</head>
+<body>
+	Website under development. Please visit us again later. <strong>Thank you.</strong>
+</body>
+</html>
