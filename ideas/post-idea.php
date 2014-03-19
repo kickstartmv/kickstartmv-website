27f8c0064c6d05b6e76e25456e168cbd5d31ae1e
nattu adnan <nattu@semicolon.com.mv>
2014-03-19 13:36:07 +0500

    Fix for ideabank

--- ideas/post-idea.php
+++ ideas/post-idea.php
@@ -10,6 +10,7 @@ $post = $_POST['Idea'];
 
 $wp = new WordPress($wpUser, $wpPass, $wpEndPoint);
 $json = array();
+
 if($wp->publish_post(htmlentities($post['title'],ENT_QUOTES), htmlentities($post['content'],ENT_QUOTES), array(), array(), array('name' => empty($post['name']) ? "Anonymous" : htmlentities($post['name'],ENT_QUOTES), 'email' => htmlentities($post['email'],ENT_QUOTES)))){
 	$json['status'] = 1;
 }
